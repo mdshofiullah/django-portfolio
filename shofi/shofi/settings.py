1fba14e965882507544f779857e34a437dd3bacc
mdshofiullah <mdshofiullah65@gmail.com>
2023-01-14 02:28:49 +0600

    Making Debug True

--- shofi/shofi/settings.py
+++ shofi/shofi/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7!##5#c_^l=m8@ghg#bca7bp593p2ioqo+&x!=7oeghbdfvisd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
