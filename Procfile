752100264c7f63b96f0e5fbe83aaf8b3436f2e09
mdshofiullah <mdshofiullah65@gmail.com>
2025-07-02 13:32:47 +0600

    update the packages and modify before deployment

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn shofi/shofi.wsgi
--- shofi/base/models.py
+++ shofi/base/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from django.utils.text import slugify
-from ckeditor.fields import RichTextField
-from ckeditor_uploader.fields import RichTextUploadingField
+
+from django_ckeditor_5.fields import CKEditor5Field
+
 
 
 # Create your models here.
@@ -15,34 +16,29 @@ class Tags(models.Model):
         return self.name
     
 
-
 class Post(models.Model):
     headline = models.CharField(max_length=200)
     sub_headline = models.CharField(max_length=200, null=True, blank=True)
     thumbnail = models.ImageField(null=True, blank=True, upload_to="images", default="p.jpg")
-    body = RichTextUploadingField(null=True, blank= True)
+    body = CKEditor5Field('Body', config_name='default', null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     activate = models.BooleanField(default=False)
     featured = models.BooleanField(default=False)
-    tags = models.ManyToManyField(Tags,null=True)
+    tags = models.ManyToManyField(Tags, blank=True)
     slug = models.SlugField(null=True, blank=True) 
     
     def __str__(self):
         return self.headline
     
-    
     def save(self, *args, **kwargs):
-        
-            if self.slug == None:
-                slug = slugify(self.headline)
-
+        if self.slug is None or self.slug == '':
+            slug = slugify(self.headline)
+            has_slug = Post.objects.filter(slug=slug).exists()
+            count = 1
+            while has_slug:
+                count += 1
+                slug = slugify(self.headline) + '-' + str(count)
                 has_slug = Post.objects.filter(slug=slug).exists()
-                count = 1
-                while has_slug:
-                    count += 1
-                    slug = slugify(self.headline) + '-' + str(count) 
-                    has_slug = Post.objects.filter(slug=slug).exists()
+            self.slug = slug
+        super().save(*args, **kwargs)
 
-                self.slug = slug
-
-            super().save(*args, **kwargs)
