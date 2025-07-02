298c928ac689d64147df6b6f9e3dd63c53e0ee9b
MD. SHOFIULLAH <mdshofiullah65@gmail.com>
2025-07-02 16:15:15 +0600

    Update settings.py
    testing purpose database change to sql database

--- shofi/shofi/settings.py
+++ shofi/shofi/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'shofi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+# DATABASES = {
+#     'default': dj_database_url.config(default=config('DATABASE_URL'))
+# }
 
 
 # Password validation
