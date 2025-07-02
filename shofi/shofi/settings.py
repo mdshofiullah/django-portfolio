e4611b708315aa06762b617713eacfa610f8afeb
mdshofiullah <mdshofiullah65@gmail.com>
2025-07-02 14:15:24 +0600

    update host on setting file

--- shofi/shofi/settings.py
+++ shofi/shofi/settings.py
@@ -14,7 +14,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 DEBUG = config('DEBUG', default=False, cast=bool)
 SECRET_KEY = config('SECRET_KEY')
 
-ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = ['*']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS = [RENDER_EXTERNAL_HOSTNAME, 'localhost']
+else:
+    ALLOWED_HOSTS = ['*']
 
 # Application definition
 
